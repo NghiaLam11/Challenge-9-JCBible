5ac4ba3a34c1247c29188000508835ba0c21b25f
hiennghia11 <lamhiennghia1110@gmail.com>
2024-09-25 21:42:46 +0700

    bible page

---
+++ src/components/BibleChilds/RelateLessonsSection.vue
@@ -0,0 +1,127 @@
+<script setup lang="ts">
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
+
+const breakpoints = {
+  // 0px and up
+  0: {
+    itemsToShow: 2,
+    snapAlign: "start",
+  },
+  // 700px and up
+  700: {
+    itemsToShow: 3,
+    snapAlign: "start",
+  },
+  // 1024 and up
+  1024: {
+    itemsToShow: 3,
+    snapAlign: "start",
+  },
+};
+</script>
+
+<template>
+  <section class="lessons">
+    <div class="container">
+      <div class="title">
+        <i class="fas fa-bible"></i>Các bài học có liên quan
+      </div>
+      <div class="list">
+        <Carousel :items-to-show="5" :breakpoints="breakpoints">
+          <Slide v-for="slide in 10" :key="slide">
+            <div class="item">
+              <div class="img">
+                <img
+                  src="/src/images/samuel-lopes-Ip5pvMPwlyo-unsplash.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="content multiline-ellipsis-2">
+                <div class="heading-content">
+                  <span>Le Tu Cam</span><span>4 ngay truoc</span>
+                </div>
+
+                <h3>Walk by faith, not by sight!</h3>
+              </div>
+            </div>
+          </Slide>
+
+          <template #addons>
+            <Navigation />
+            <Pagination />
+          </template>
+        </Carousel>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.lessons {
+  padding: 3rem calc(100% / 5);
+}
+.lessons .title {
+  display: flex;
+  align-items: center;
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin-left: 1rem;
+  margin-bottom: 0.5rem;
+}
+.lessons .title i {
+  margin-right: 0.4rem;
+  color: var(--warning-color);
+  font-size: 1.4rem;
+}
+.lessons .list {
+  margin: 0 0.5rem;
+}
+.lessons .item {
+  padding: 0.5rem;
+  cursor: pointer;
+}
+.lessons .item:hover > .content h3 {
+  color: var(--success-color);
+}
+.lessons .item .img img {
+  border-radius: var(--border-radius);
+}
+.lessons .item .content {
+  text-align: start;
+  padding: 0.2rem;
+}
+.lessons .item .content span {
+  font-size: 0.7rem;
+}
+.lessons .item .content span:last-child {
+  opacity: 0.5;
+  font-weight: 200;
+}
+.lessons .item .content .heading-content {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.lessons .item .content .heading-content span:first-child {
+  font-weight: bold;
+  color: var(--text-color);
+}
+.lessons .item .content h3 {
+  line-height: 1.2rem;
+}
+@media screen and (max-width: 768px) {
+  .lessons {
+    padding: 3rem calc(100% / 10);
+  }
+}
+@media screen and (max-width: 538px) {
+  .lessons {
+    padding: 1rem;
+  }
+  .lessons .item .content h3 {
+    line-height: 1.1rem;
+    font-size: 0.9rem;
+  }
+}
+</style>
--- src/components/BibleSection.vue
+++ src/components/BibleSection.vue
@@ -1,23 +1,79 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import RelateLessonsSection from "./BibleChilds/RelateLessonsSection.vue";
+const isShowBooks = ref(false);
+const isShowVersions = ref(false);
+const isShowTextSize = ref(false);
+const onShowBooks = () => {
+  isShowBooks.value = !isShowBooks.value;
+};
+const onShowVersions = () => {
+  isShowVersions.value = !isShowVersions.value;
+};
+const onShowTextSize = () => {
+  isShowTextSize.value = !isShowTextSize.value;
+};
+const indexOfBook = ref(1);
+const onShowChapters = (index: number) => {
+  if (indexOfBook.value != index) {
+    indexOfBook.value = index;
+  } else {
+    indexOfBook.value = 0;
+  }
+};
+</script>
 
 <template>
   <div class="container">
     <div class="cate">
-      <select>
-        <option value="Sáng thế ký">Sáng thế ký</option>
-        <option value="Xuat E-dip-to ký">Xuat E-dip-to ký</option>
-      </select>
-      <select>
-        <option value="Sáng thế ký">VIE2010</option>
-        <option value="Xuat E-dip-to ký">VIE1925</option>
-      </select>
+      <ul class="select">
+        <h4 @click="onShowBooks">
+          <span>Sáng thế ký</span
+          ><i class="fas fa-caret-up" v-if="isShowBooks"></i>
+          <i v-else class="fas fa-caret-down"></i>
+        </h4>
+        <ul class="list-hidden" v-if="isShowBooks">
+          <li v-for="m in 3" :key="m" @click="onShowChapters(m)">
+            <h5>Sáng thế ký sáng thế ký</h5>
+            <ul class="chapters" v-if="indexOfBook == m">
+              <li v-for="n in 15" :key="n">{{ n }}</li>
+            </ul>
+          </li>
+        </ul>
+      </ul>
+      <ul class="select">
+        <h4 @click="onShowVersions">
+          <span>VIE2010</span
+          ><i class="fas fa-caret-up" v-if="isShowVersions"></i
+          ><i v-else class="fas fa-caret-down"></i>
+        </h4>
+        <ul class="list-hidden" v-if="isShowVersions">
+          <li value="Xuat E-dip-to ký">VIE1925</li>
+          <li value="Sáng thế ký">VIE2010</li>
+          <li value="Xuat E-dip-to ký">VIE1925</li>
+        </ul>
+      </ul>
+      <ul class="select">
+        <h4 @click="onShowTextSize">
+          <span>20px</span><i class="fas fa-caret-up" v-if="isShowTextSize"></i
+          ><i v-else class="fas fa-caret-down"></i>
+        </h4>
+        <ul class="list-hidden" v-if="isShowTextSize">
+          <li>10px</li>
+          <li>20px</li>
+          <li>30px</li>
+          <li>40px</li>
+          <li>50px</li>
+          <li>60px</li>
+        </ul>
+      </ul>
     </div>
     <div class="bible">
       <button class="prev"><i class="fas fa-angle-left"></i></button
       ><button class="next"><i class="fas fa-angle-right"></i></button>
       <h4>Sang the ky 1</h4>
       <div class="para">
-        <span v-for="n in 30" :key="n"
+        <span v-for="n in 20" :key="n"
           ><b
             ><sup
               ><i>{{ n }} </i>
@@ -29,12 +85,75 @@
         >
       </div>
     </div>
+    <RelateLessonsSection></RelateLessonsSection>
   </div>
 </template>
 
 <style scoped>
 .cate {
   margin: 2rem 1rem;
+  display: flex;
+  flex-wrap: wrap;
+}
+.cate .select {
+  list-style: none;
+  position: relative;
+  padding-left: 0;
+  min-width: 150px;
+  margin: 0.5rem;
+  border: 1px solid var(--border-color);
+  border-radius: var(--border-radius);
+}
+.cate .select:first-child {
+  min-width: 250px;
+}
+.cate .select h4 {
+  padding: 0.5rem 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
+}
+.cate .select h4 span {
+  -webkit-line-clamp: 1;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  white-space: pre-wrap;
+}
+.cate .select .list-hidden {
+  position: absolute;
+  list-style: none;
+  padding-left: 0;
+  border: 1px solid var(--border-color);
+  background-color: var(--bg-primary);
+  width: 100%;
+  z-index: 10;
+}
+.cate .select .list-hidden li {
+  cursor: pointer;
+  padding: 0.3rem 1rem;
+  border-bottom: 1px solid var(--blur-color);
+  opacity: 0.8;
+  overflow: hidden;
+}
+.cate .select .list-hidden li:hover {
+  opacity: 1;
+}
+.cate .select .list-hidden li h5 {
+  font-size: 1rem;
+  padding: 0.1rem 0;
+  opacity: 0.9;
+}
+.cate .select .list-hidden li .chapters {
+  display: flex;
+  justify-content: start;
+  flex-wrap: wrap;
+  padding-left: 0;
+}
+.cate .select .list-hidden li .chapters li {
+  border: 1px solid var(--blur-color);
+  font-size: 0.8rem;
 }
 .bible {
   padding: 1rem calc(100% / 5);
@@ -47,12 +166,11 @@
   line-height: 1.8rem;
   text-align: center;
   padding: 1rem 0;
-  border-bottom: 2px solid var(--border-color);
 }
 .bible .next,
 .prev {
   position: fixed;
-  top: 50%;
+  top: 60%;
   background-color: var(--bg-primary);
   color: var(--text-color);
   border: 1px solid var(--border-color);
@@ -61,6 +179,7 @@
   height: 50px;
   border-radius: 50%;
   opacity: 0.7;
+  cursor: pointer;
 }
 .bible .next {
   right: 10%;
@@ -73,7 +192,20 @@
   letter-spacing: 0.6px;
   text-align: justify;
   color: var(--text-color);
-  height: 100vh;
-  overflow: scroll;
+  font-size: 20px;
+}
+@media screen and (max-width: 768px) {
+  .bible {
+    padding: 1rem calc(100% / 10);
+  }
+}
+@media screen and (max-width: 538px) {
+  .bible {
+    padding: 1rem;
+  }
+  .bible .next,
+  .prev {
+    top: 80%;
+  }
 }
 </style>
